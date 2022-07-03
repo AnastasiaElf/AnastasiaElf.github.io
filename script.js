b1870b703bf63aa5e52ee106f908499b84e56c69
AnastasiaElf <anastasia.elf.17@gmail.com>
2022-07-03 23:42:51 +0200

    fix adaptivity

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = updateLinkContentsWidth;
 
 function updateLinkContentsWidth() {
-    let elements = document.querySelectorAll(".elf-link .elf-link-content");
-    let elementsWidth = Array.from(elements, (elem) => elem.offsetWidth);
-    let maxWidth = Math.max(...elementsWidth);
+    if (window.screen.width <= 700) {
+        let elements = document.querySelectorAll(".elf-link .elf-link-content");
+        let elementsWidth = Array.from(elements, (elem) => elem.offsetWidth);
+        let maxWidth = Math.max(...elementsWidth);
 
-    elements.forEach((elem) => (elem.style.width = `${maxWidth + 1}px`));
+        elements.forEach((elem) => (elem.style.width = `${maxWidth + 1}px`));
+    }
 }
