8d5ffda679c3fc617320e33daf2b23c6606bbe25
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-05-03 10:36:40 +0200

    remove old content

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 window.onload = () => {
-    // updateLinkContentsWidth();
     enableCopyOnClick();
 };
 
-// function updateLinkContentsWidth() {
-//     if (window.screen.width <= 700) {
-//         let elements = document.querySelectorAll(".elf-link .elf-link-content");
-//         let elementsWidth = Array.from(elements, (elem) => elem.offsetWidth);
-//         let maxWidth = Math.max(...elementsWidth);
-
-//         elements.forEach((elem) => (elem.style.width = `${maxWidth + 1}px`));
-//     }
-// }
-
 function enableCopyOnClick() {
-    let linkElem = document.querySelector("#elf-discord-link");
-    let textElem = linkElem.querySelector(".elf-link-text");
+    let linkElem = document.querySelector("#ana-discord-link");
+    let textElem = linkElem.querySelector(".ana-link-text");
     let timeout;
 
     linkElem.addEventListener("click", () => {
